6422228c3714c876a7576c917a551fd727c78c4e
Taylor Buchheit <tbuchhei@gmail.com>
2015-06-17 19:51:14 -0700

    Final Update. Through section 15

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,7 +1,7 @@
 #Rails.application.routes.draw do
 Blogger::Application.routes.draw do
  
-  root to: 'articiles#index'
+  root to: 'articles#index'
   resources :articles do
     resources :comments
   end
